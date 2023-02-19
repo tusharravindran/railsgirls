f901ec24df2bfcc16d9307a25e0a06411e05cb6b
tushar R <tusharrravindran@gmail.com>
2023-02-19 22:05:02 +0530

    Validation and associaation

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,5 @@
 class Idea < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   validates_presence_of :name,:description
   mount_uploader :picture, PictureUploader
 end
