19341f826807fbb69a1629acfee5ca51be6a0ccc
tushar R <tusharrravindran@gmail.com>
2023-02-20 11:02:17 +0530

    scope for signout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+  end
   root "pages#home"
   get "pages/about"
   resources :ideas do
